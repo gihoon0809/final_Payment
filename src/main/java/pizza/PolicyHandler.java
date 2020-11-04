d1fc2ea378fca8583134a6d735be15cad1259f19
gihoon0809 <gihoon0809@gmail.com>
2020-11-04 14:54:11 +0900

    upper

--- src/main/java/pizza/PolicyHandler.java
+++ src/main/java/pizza/PolicyHandler.java
@@ -10,6 +10,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PolicyHandler{
+
+    // LDH 소스추가
+    @Autowired
+    PaymentRepository PaymentRepository;
+
     @StreamListener(KafkaProcessor.INPUT)
     public void onStringEventListener(@Payload String eventString){
 
@@ -19,6 +24,14 @@ public class PolicyHandler{
     public void wheneverOrderCanceled_CancelPayment(@Payload OrderCanceled orderCanceled){
 
         if(orderCanceled.isMe()){
+
+            // 소스추가 LDH
+            Payment payment = new Payment();
+            payment.setOrderId(orderCanceled.getId());
+            payment.setPaymentStatus("Canceled");
+
+            PaymentRepository.save(payment);
+
             System.out.println("##### listener CancelPayment : " + orderCanceled.toJson());
         }
     }
